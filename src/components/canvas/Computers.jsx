7a77dcbcf81b47dd08ff7ae1a88b1d2b15c8f1d7
Carlos Azpurua <carlosazpurua1@gmail.com>
2023-07-12 22:05:03 -0300

    feat: big long shot

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,14 +4,14 @@ import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import CanvasLoader from "../Loader";
 
-const Computers = ({ isMobile=false }) => {
+const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor='black' />
       <spotLight
-        position={[-20, 50, 10]}
+        position={[-25, 50, 10]}
         angle={0.12}
         penumbra={1}
         intensity={1}
@@ -21,8 +21,8 @@ const Computers = ({ isMobile=false }) => {
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        scale={isMobile ? 0.7 : 0.85}
+        position={isMobile ? [0, -3, -2.2] : [0, -4, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -69,7 +69,6 @@ const ComputersCanvas = () => {
         />
         <Computers isMobile={isMobile} />
       </Suspense>
-
       <Preload all />
     </Canvas>
   );
--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,14 @@
-import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
-import Hero from './Hero';
-import Navbar from './Navbar';
-import About from './About';
-import Tech from './Tech';
-import Experience from './Experience';
-import Works from './Works';
-import Feedbacks from './Feedbacks';
-import Contact from './Contact';
+import { BallCanvas, ComputersCanvas, EarthCanvas, StarsCanvas } from './canvas';
+
+import About from "./About";
+import CanvasLoader from "./Loader";
+import Contact from "./Contact";
+import Experience from "./Experience";
+import Feedbacks from "./Feedbacks";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Tech from "./Tech";
+import Works from "./Works";
 
 export {
   Hero,
@@ -17,8 +19,9 @@ export {
   Works,
   Feedbacks,
   Contact,
+  CanvasLoader,
   EarthCanvas, 
   BallCanvas, 
   ComputersCanvas, 
   StarsCanvas
-}
+};
